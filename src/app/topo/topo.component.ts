217cdcfa5c61e5c96a3ef662dd400a744783cfff
wsrmelo <willany_lp@hotmail.com>
2018-06-08 11:54:43 -0300

    Firt Commit

---
+++ src/app/topo/topo.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-topo',
+  templateUrl: './topo.component.html',
+  styleUrls: ['./topo.componet.css']
+})
+export class TopoComponent { }

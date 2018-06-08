8706b1b0f141cf82d3f870cd1633556ac205eed4
wsrmelo <willany_lp@hotmail.com>
2018-06-08 14:47:54 -0300

    validating the answer

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -12,8 +12,12 @@ export class PainelComponent implements OnInit {
   public frases: Frase[] = FRASES
   public instrucao: string = "Traduza a frase:"
   public resposta: string
+  public rodada: number = 0
+  public rodadaFrase: Frase
 
-  constructor() { }
+  constructor() {
+    this.rodadaFrase = this.frases[this.rodada]
+  }
 
   ngOnInit() {
   }
@@ -23,7 +27,12 @@ export class PainelComponent implements OnInit {
   }
 
   public verificarResposta(): void{
-    console.log("verificando a resposta: ", this.resposta)
+    if(this.rodadaFrase.frasePtBr == this.resposta){
+      //trocar pergunta da rodadaFrase
+      this.rodada++
+      this.rodadaFrase = this.frases[this.rodada]
+    }
+
   }
 
 }
